b1f5170259f05df5b758ebc0179a1dabe03dfb48
Luis Ángel Méndez Gort <gort.andres000@gmail.com>
2018-01-16 17:40:18 -0500

    Test deleted due to difficulty in updating. More info queried.

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 #Sigapi
 
 Sigapi es un simple servidor HTTP para servir la tabla `student` de SIGENU. La consulta que hace es `SELECT id_student, t,identification,name,middle_name,last_name,address,phone FROM student LIMIT X OFFSET Y` donde X y Y deben ser proporcionados por el usuario. Las restricciones `LIMIT X` y `OFFSET Y` sirven para paginar. Con la API corriendo en `localhost:8080` la URL para consultar es `http://localhost:8080/?offset=Y&size=X`, donde `X` e `Y` deben reemplazarse por números.
+
+## Despliegue
+
+```conf
+[Unit]
+Description=Sigapi Service
+After=network.target
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/root
+ExecStart=/usr/local/bin/sigapi -a postgresql.upr.edu.cu/sigenu -u usuario -p contraseña -s :8081
+Restart=on-abort
+
+[Install]
+WantedBy=multi-user.target
+```
--- sigapi.go
+++ sigapi.go
@@ -19,7 +19,10 @@ type DBRecord struct {
 	//address
 	Addr string `json:"addr"`
 	//telephone number
-	Tel string `json:"tel"`
+	Tel     string `json:"tel"`
+	Career  string `json:"career"`
+	Faculty string `json:"faculty"`
+	Status  string `json:"status"`
 }
 
 func NewPostgreSDB(addr, user, pass string) (d *SDB, e error) {
@@ -27,32 +30,13 @@ func NewPostgreSDB(addr, user, pass string) (d *SDB, e error) {
 	db, e = sql.Open("postgres",
 		fmt.Sprintf("postgres://%s:%s@%s", user, pass, addr))
 	if e == nil {
-		d = &SDB{Db: &sqQr{db: db}}
+		d = &SDB{Db: db}
 	}
 	return
 }
 
-type sqQr struct {
-	db *sql.DB
-}
-
-func (s *sqQr) Query(q string,
-	args ...interface{}) (c Scanner, e error) {
-	c, e = s.db.Query(q, args...)
-	return
-}
-
 type SDB struct {
-	Db Querier
-}
-
-type Querier interface {
-	Query(string, ...interface{}) (Scanner, error)
-}
-
-type Scanner interface {
-	Next() bool
-	Scan(...interface{}) error
+	Db *sql.DB
 }
 
 func (d *SDB) ServeHTTP(w h.ResponseWriter, r *h.Request) {
@@ -82,25 +66,98 @@ func writeErr(w h.ResponseWriter, e error) {
 }
 
 func (d *SDB) query(offset, size string) (s []DBRecord, e error) {
+	// facultad, graduado
 	query := fmt.Sprintf(
 		"SELECT id_student,identification,name,"+
-			"middle_name,last_name,address,phone FROM student LIMIT %s"+
-			" OFFSET %s",
+			"middle_name,last_name,address,phone,student_status_fk,"+
+			"faculty_fk,career_fk FROM student LIMIT %s OFFSET %s",
 		size,
 		offset)
-	var r Scanner
+	var r *sql.Rows
 	r, e = d.Db.Query(query)
-	b, s := e == nil && r.Next(), make([]DBRecord, 0)
-	for b {
+	s = make([]DBRecord, 0)
+	for e == nil && r.Next() {
 		st := DBRecord{}
-		var name, middle_name, last_name string
+		var name, middle_name, last_name, car_fk string
+		var stat_fk, fac_fk sql.NullString
 		e = r.Scan(&st.Id, &st.IN, &name, &middle_name, &last_name,
-			&st.Addr, &st.Tel)
+			&st.Addr, &st.Tel, &stat_fk, &fac_fk, &car_fk)
+		var ax *auxInf
 		if e == nil {
+			ax, _ = d.queryAux(stat_fk.String, fac_fk.String, car_fk)
+		}
+		if e == nil {
+			st.Career, st.Faculty, st.Status = ax.career, ax.faculty,
+				ax.status
 			st.Name = name + " " + middle_name + " " + last_name
 			s = append(s, st)
 		}
-		b = e == nil && r.Next()
+	}
+	if r != nil && e == nil {
+		e = r.Err()
+		r.Close()
+	}
+	return
+}
+
+type auxInf struct {
+	status  string
+	faculty string
+	career  string
+}
+
+func (d *SDB) queryAux(stat_fk, fac_fk,
+	car_fk string) (f *auxInf, e error) {
+	f = new(auxInf)
+	q, a, r := []string{
+		"SELECT kind FROM student_status WHERE id_student_status = ",
+		"SELECT name FROM faculty WHERE id_faculty = ",
+		"SELECT national_career_fk FROM career WHERE id_career = ",
+	},
+		[]string{
+			stat_fk,
+			fac_fk,
+			car_fk,
+		},
+		make([]sql.NullString, 3)
+
+	for i := 0; e == nil && i != len(q); i++ {
+		qr := q[i] + "'" + a[i] + "'"
+		s, e := d.Db.Query(qr)
+		if e == nil {
+			if s.Next() {
+				e = s.Scan(&r[i])
+			} else {
+				e = fmt.Errorf("Información no encontrada")
+			}
+		}
+		if e == nil {
+			e = s.Err()
+			s.Close()
+		}
+	}
+	var s *sql.Rows
+	if e == nil {
+		f.status = r[0].String
+		f.faculty = r[1].String
+		if r[2].Valid {
+			ncq := "SELECT name FROM national_career" +
+				" WHERE id_national_career = '" + r[2].String + "'"
+			s, e = d.Db.Query(ncq)
+		}
+	}
+	if e == nil {
+		if r[2].Valid {
+			if s.Next() {
+				e = s.Scan(&f.career)
+			} else {
+				e = fmt.Errorf("Información no encontrada")
+			}
+		}
+	}
+	if e == nil && s != nil {
+		e = s.Err()
+		s.Close()
 	}
 	return
 }
